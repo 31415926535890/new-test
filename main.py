1fe249a29ee074ac4b1f87c4a1ce02e564d52e22
31415926535890 <149445592+31415926535890@users.noreply.github.com>
2023-10-30 18:46:28 -0600

    Restore '10/30/2023, 6:45:44 PM Initialize empty repo'

--- main.py
+++
@@ -1,7 +0,0 @@
-@namespace
-class SpriteKind:
-    coin = SpriteKind.create()
-
-def on_overlap_tile(sprite, undefined):
-    pass
-scene.on_overlap_tile(SpriteKind.player, None, on_overlap_tile)
--- main.ts
+++
@@ -1,6 +0,0 @@
-namespace SpriteKind {
-    export const coin = SpriteKind.create()
-}
-scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, undefined) {
-	
-})
